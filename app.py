c52c783bb5874b96c89236b5bda1d6efac19a8ae
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2023-10-21 17:29:06 -0400

    Update app.py
    Port 80

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ if __name__ == '__main__':
     observer.schedule(event_handler, path='.', recursive=True)
     observer.start()
 
-    app.run(host='0.0.0.0', port=443, threaded=True)
+    app.run(host='0.0.0.0', port=80, threaded=True)
