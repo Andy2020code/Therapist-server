86b76e4df15e72f4aa88b6d68ddc16ba263ce92e
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2023-10-21 00:54:09 -0400

    change to flask
    changeto flask

--- app.py
+++ app.py
@@ -1,47 +1,45 @@
-from bottle import Bottle, run, static_file
+from flask import Flask, render_template
 import sys
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-app = Bottle()
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World! This is a Bottle WSGI application.'
-
-@app.route('/html/<filename>')
-def serve_html(filename):
-    return static_file(filename, root='./html/')
+    return 'Hello, World! This is a Flask application.'
 
 # Add a route to serve your CSS file
 @app.route('/css/<filename>')
 def serve_css(filename):
-    return static_file(filename, root='./css/')
+    return app.send_static_file('css/' + filename)
 
+# Add a route to serve your image files
 @app.route('/IMG/<filename>')
 def serve_images(filename):
-    return static_file(filename, root='./IMG/')
+    return app.send_static_file('IMG/' + filename)
 
+# Add a route to serve your JavaScript files
 @app.route('/js/<filename>')
 def serve_js(filename):
-    return static_file(filename, root='./js/')
+    return app.send_static_file('js/' + filename)
 
 # Add routes for specific HTML pages
 @app.route('/index')
 def serve_index():
-    return static_file('index.html', root='./html/')
+    return render_template('index.html')
 
 @app.route('/our-services')
 def serve_our_services():
-    return static_file('our-services.html', root='./html/')
+    return render_template('our-services.html')
 
 @app.route('/therapists')
 def serve_therapists():
-    return static_file('therapists.html', root='./html/')
+    return render_template('therapists.html')
 
 @app.route('/take-action')
 def serve_take_action():
-    return static_file('take-action.html', root='./html/')
+    return render_template('take-action.html')
 
 if __name__ == '__main__':
     if sys.platform == 'win32':
@@ -54,7 +52,7 @@ if __name__ == '__main__':
         print("Change detected. Restarting the server.")
         observer.stop()
         observer.join()
-        run(app, host='0.0.0.0', port=8000)
+        app.run(host='0.0.0.0', port=8000, threaded=True)
 
     # Create a watchdog event handler
     class FileChangeHandler(FileSystemEventHandler):
@@ -67,4 +65,4 @@ if __name__ == '__main__':
     observer.schedule(event_handler, path='.', recursive=True)
     observer.start()
 
-    run(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
