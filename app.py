b7f274ca0218347d5d394e0f5f95dfdeeaafba2a
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2023-10-21 22:22:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-import sys
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from threading import Thread
-from waitress import serve  # Import Waitress
+import gunicorn.app.base
+from gunicorn.six import iteritems
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def index():
@@ -44,11 +44,6 @@ def serve_take_action():
     return render_template('take-action.html')
 
 if __name__ == '__main__':
-    if sys.platform == 'win32':
-        # Windows-specific code
-        import msvcrt
-        # Perform Windows-specific file operations using msvcrt
-
     # Function to handle changes and restart the server
     def on_change(event):
         print("Change detected. Restarting the server.")
@@ -58,7 +53,30 @@ if __name__ == '__main__':
         serve_thread.start()
 
     def serve_app():
-        serve(app, host='0.0.0.0', port=5000)  # Use Waitress to serve the app
+        # Use Gunicorn to serve the app
+        options = {
+            'bind': '0.0.0.0:5000',
+            'workers': 4  # You can adjust the number of workers as needed
+        }
+
+        class StandaloneApplication(gunicorn.app.base.BaseApplication):
+            def __init__(self, app, options=None):
+                self.application = app
+                self.options = options or {}
+                super(StandaloneApplication, self).__init__()
+
+            def load_config(self):
+                config = {
+                    key: value for key, value in iteritems(self.options)
+                    if key in self.cfg.settings and value is not None
+                }
+                for key, value in iteritems(config):
+                    self.cfg.set(key, value)
+
+            def load(self):
+                return self.application
+
+        StandaloneApplication(app, options).run()
 
     # Create a watchdog event handler
     class FileChangeHandler(FileSystemEventHandler):
@@ -71,5 +89,6 @@ if __name__ == '__main__':
     observer.schedule(event_handler, path='.', recursive=True)
     observer.start()
 
+    # Use Gunicorn to serve the app
     serve_thread = Thread(target=serve_app)
     serve_thread.start()
